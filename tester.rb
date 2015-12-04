6e6862b331f8e0856753682a7c1be8de0ea79cf2
Nathan Rapport <nathanrapport@Nathans-MacBook-Air.local>
2015-12-04 13:18:47 -0800

    changed tester.rb

--- tester.rb
+++ tester.rb
@@ -1,7 +1,7 @@
 File.foreach(ARGV.first) do |line|
   dev_str, design_str = line.split('|').map(&:strip).map{|x| x.split(//)}
   nbugs = dev_str.zip(design_str).select{|x,y| x!=y}.length
-  result = case nbugs
+  puts case nbugs
     when 0 then "Done"
     when 1..2 then "Low"
     when 3..4 then "Medium"
@@ -9,5 +9,4 @@ File.foreach(ARGV.first) do |line|
     else 
       "Critical"
   end
-  puts result
 end
