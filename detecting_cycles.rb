483a08e188ad98825f0ba17b4f12fbca5e7df41e
Nathan Rapport <n.rapport@gmail.com>
2015-12-12 03:24:27 -0800

    modified detecting cycles

--- detecting_cycles.rb
+++ detecting_cycles.rb
@@ -1,12 +1,29 @@
+# this algo is not quite right... need to solve it for cases where cycle repeats toward very end
 File.foreach(ARGV[0]) do |line|
   data=line.chomp.split(" ").map{|x| x.to_i}
-  cycles=chunks=[]
-  [*0..data.length].each do |i|
-    [*1..data.length].each do |j|
-      chunks<<data[i..j]
+
+  # find the period
+  i = 0
+  tortoise = data[0]
+  hare = data[1]
+  period = 1
+  while tortoise != hare
+    i+=1
+    tortoise = data[i]
+    hare = data[2*i]
+    period = i if tortoise == hare
+  end
+
+  # find the cycle
+  cycle = (period == data.length) ? [data[-1]] : []
+  data.each_with_index do |n, i|
+    from = data[i]  
+    to = data[i+period]
+    if to == from
+      cycle = data[i...i+period] 
+      break
     end
   end
-  cycles<<chunks.uniq.find{|x| chunks.count(x)>1}
-  p cycles
+  puts cycle.join(" ")
 end
 
