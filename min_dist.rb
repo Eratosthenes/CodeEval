a9babf5128c51ee68a880af326cb9aee7a90a1a4
Nathan Rapport <n.rapport@gmail.com>
2015-12-12 15:07:17 -0800

    fixed min dist

--- min_dist.rb
+++ min_dist.rb
@@ -1,14 +1,15 @@
-def min_dist(*args)
+def min_dist(args)
   a=[*args.min..args.max]
-  min_dist = a.length*a[-1]
+  min_dist = a.length*args.max
   a.each do |n|
     dist = args.map{|x| (x-n).abs}.reduce(:+)
+    #p "dist(#{n}) = #{dist}"
     min_dist = dist if dist < min_dist
   end
   min_dist
 end
 
 File.foreach(ARGV.first) do |line|
-  args = line.chomp.split(" ").map{|x| x.to_i}
-  puts min_dist(*args)
+  args = line.chomp.split(" ").map(&:to_i).drop(1)
+  puts min_dist(args)
 end
