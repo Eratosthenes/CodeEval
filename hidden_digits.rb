bb96e634876813a3e7789f188c2cd1b18910fc5a
Nathan Rapport <n.rapport@gmail.com>
2015-12-12 13:00:06 -0800

    modified hidden digits

--- hidden_digits.rb
+++ hidden_digits.rb
@@ -1,7 +1,7 @@
-$h = Hash[[*?a..?z].zip(*[0..25])]
+$h = Hash[[*?a..?j].zip(*[0..9])]
 File.foreach(ARGV.first) do |line|
   res=[];line.chomp.split(//).each do |letter|
-    res << $h[letter] if [*?a..?z].include?(letter)
+    res << $h[letter] if [*?a..?j].include?(letter)
     res << letter if [*?0..?9].include?(letter)
   end
   puts res==[] ? "NONE" : res.join()
