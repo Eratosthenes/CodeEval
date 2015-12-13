24d8ef463ce41e96c7ab5bbc3faa308abafd63c8
Nathan Rapport <n.rapport@gmail.com>
2015-12-12 20:56:41 -0800

    fixed closest pair

--- closest_pair.rb
+++ closest_pair.rb
@@ -1,8 +1,24 @@
-contents = File.readlines(ARGV.first).map{|x| x.chomp}
-points = contents[1..-2].map{|x| x.split(" ").map{|y| y.to_i}}
-pairs = points.combination(2).to_a
-dists = []; pairs.each do |pair|
-  x1,y1,x2,y2 = pair.flatten
-  dists << (y2-y1)**2 + (x2-x1)**2
+def min_dist(chunk)
+  points = chunk.map{|x| x.split(" ").map(&:to_i)}
+  dists = []
+  points.each do |p1|
+    x1,y1 = p1.flatten
+    points.each do |p2|
+      x2,y2 = p2.flatten
+      dists << Math.sqrt((y2-y1)**2 + (x2-x1)**2) if p1!=p2
+    end
+  end
+  dists.min<10000 ? "%.4f"%dists.min : "INFINITY"
 end
-puts "%.4f"%Math.sqrt(dists.min)
+
+#main
+contents = File.readlines(ARGV.first).map{|x| x.chomp}
+i = 0
+loop do
+  chunk_size = contents[i].to_i
+  break if chunk_size == 0
+  chunk = contents[i+1..i+chunk_size]
+  puts min_dist(chunk)
+  i += chunk_size+1
+end
+
