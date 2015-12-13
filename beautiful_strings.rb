0f89747c2f6b5b3b15e52ca33a213472b82400ef
Nathan Rapport <n.rapport@gmail.com>
2015-12-12 18:23:05 -0800

    beautiful_strings.rb

--- beautiful_strings.rb
+++ beautiful_strings.rb
@@ -2,7 +2,7 @@ File.foreach(ARGV[0]) do |line|
   text = line.chomp.downcase.gsub(/[^a-z]/,"").split(//)
   res = []
   text.uniq.each do |letter|
-    res << text.partition{|x| x==letter}[0].length
+    res << text.count(letter)
   end
   pairs = res.sort.reverse.zip([*1..26].reverse)
   puts pairs.map{|x,y| x*y}.reduce(:+)
