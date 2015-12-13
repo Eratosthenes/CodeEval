80a7cfe19420d6b95bea42596024c8aab81acff9
Nathan Rapport <n.rapport@gmail.com>
2015-12-12 23:42:10 -0800

    fixed mth to last

--- mth_to_last.rb
+++ mth_to_last.rb
@@ -1,8 +1,5 @@
 File.foreach(ARGV[0]) do |line|
-  data, m = line.chomp.split(" ")[0...-1], line.chomp.split(//)[-1].to_i
-  if m < data.length 
-    puts data[m] 
-  else 
-    puts data[0]
-  end
+  line = line.chomp.split(" ")
+  data, n = line[0...-1], line[-1].to_i
+  puts data.reverse[n-1]
 end
