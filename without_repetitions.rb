38661e82ec4a5c5f9163955864d56523d096149a
Nathan Rapport <n.rapport@gmail.com>
2015-12-14 03:20:39 -0800

    did it with gsub (much quicker)

--- without_repetitions.rb
+++ without_repetitions.rb
@@ -1,25 +1,30 @@
-#there is probably a better way to do this, but oh well
+#short way
 File.foreach(ARGV[0]) do |line|
   words = line.chomp.split(' ')
-
-  res = [] 
+  res = []
   words.each do |word|
-    word = word.split(//)
-    deletions = []
-    word.each_with_index do |letter, i|
-      next if i==0
-      deletions << i if word[i]==word[i-1]
-    end
-
-    count = 0
-    deletions.each do |i|
-      word.delete_at(i-count)
-      count+=1
-    end
-
-    word = word.join()
-    res << word
+    res << word.gsub(/(\w)\1+/,'\1')
   end
+
   puts res.join(' ')
 end
+# stupid way (without gsub)
+#  res = [] 
+#  words.each do |word|
+#    word = word.split(//)
+#    deletions = []
+#    word.each_with_index do |letter, i|
+#      next if i==0
+#      deletions << i if word[i]==word[i-1]
+#    end
+#
+#    count = 0
+#    deletions.each do |i|
+#      word.delete_at(i-count)
+#      count+=1
+#    end
+#
+#    word = word.join()
+#    res << word
+#  end
 
