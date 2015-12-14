06c2da5e7cc5fce7d38e8274fad51868278ce312
Nathan Rapport <n.rapport@gmail.com>
2015-12-14 02:06:08 -0800

    fixed compare points and multiples of number

--- compare_points.rb
+++ compare_points.rb
@@ -1,25 +1,27 @@
 def hiking(x1,y1,x2,y2)
- return "here" if x1==x2 && y1==y2
- return "N" if y2-y1>0 && x2-x1==0
- return "S" if y2-y1<0 && x2-x1==0
- theta = Math.atan((y2-y1)/(x2-x1).to_f)
  return case 
- when y2-y1==0 && x2-x1>0
+ when y2==y1 && x2==x1
+  "here"
+ when y2>y1 && x2==x1
+  "N"
+ when y2<y1 && x2==x1
+  "S"
+  when y1==y2 && x2>x1
   "E"
- when theta.between?(0, Math::PI/2)
-  "NE"
- when theta.between?(Math::PI/2, Math::PI)
-  "NW"
- when y2-y1==0 && x2-x1<0
+ when y1==y2 && x2<x1
   "W"
- when theta.between?(Math::PI, 3*Math::PI/2)
+ when x2>x1 && y2>y1
+  "NE"
+ when x2<x1 && y2>y1
+  "NW"
+  when x2<x1 && y2<y1
   "SW"
- when theta.between?(3*Math::PI/2, 2*Math::PI)
+ when x2>x1 && y2<y1
   "SE"
  end
 end
 
 File.foreach(ARGV[0]) do |line|
-  x1, y1, x2, y2 = line.chomp.split(' ').map(&:to_i)
-  puts hiking(x1, y1, x2, y2)
+  x1, y1, x2, y2 = line.split(' ').map(&:to_i)
+  puts hiking(x1,y1,x2,y2)
 end
--- multiples_of_number.rb
+++ multiples_of_number.rb
@@ -1,7 +1,6 @@
 File.foreach(ARGV[0]) do |line|
   target, n = line.chomp.split(',').map(&:to_i)
-  until n >= target
-    n+=n
-  end
-  puts n
+  m=n
+  m+=n while m < target
+  puts m
 end
